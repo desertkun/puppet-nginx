d94c4ef2f195d4672447c859365991fe27c46318
Matthew Haughton <matt@3flex.com.au>
2016-08-05 13:21:02 -0400

    rubocop fixes

--- spec/unit/facter/util/fact_nginx_version_spec.rb
+++ spec/unit/facter/util/fact_nginx_version_spec.rb
@@ -1,28 +1,28 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe Facter::Util::Fact do
-  before {
+  before do
     Facter.clear
-  }
+  end
 
-  describe "nginx_version" do
+  describe 'nginx_version' do
     context 'with current version output format' do
       before :each do
         Facter::Util::Resolution.stubs(:which).with('nginx').returns(true)
         Facter::Util::Resolution.stubs(:exec).with('nginx -v 2>&1').returns('nginx version: nginx/1.8.1')
       end
-      it {
+      it do
         expect(Facter.fact(:nginx_version).value).to eq('1.8.1')
-      }
+      end
     end
     context 'with old version output format' do
       before :each do
         Facter::Util::Resolution.stubs(:which).with('nginx').returns(true)
         Facter::Util::Resolution.stubs(:exec).with('nginx -v 2>&1').returns('nginx: nginx version: nginx/0.7.0')
       end
-      it {
+      it do
         expect(Facter.fact(:nginx_version).value).to eq('0.7.0')
-      }
+      end
     end
   end
 end
